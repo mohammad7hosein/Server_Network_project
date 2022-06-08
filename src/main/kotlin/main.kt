f6044b5b5453117eb4479a0563d00ada07feba46
mohammad7hosein <mohamad77hosein77@gmail.com>
2022-06-08 19:51:21 +0430

    receive data from client

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,58 +1,100 @@
 import androidx.compose.desktop.DesktopMaterialTheme
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.platform.Font
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDirection
+import androidx.compose.ui.unit.ExperimentalUnitApi
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
-import kotlinx.coroutines.Dispatchers.IO
-import kotlinx.coroutines.Dispatchers.Main
-import kotlinx.coroutines.async
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withContext
-import java.io.OutputStream
 import java.net.ServerSocket
 import java.nio.charset.Charset
-import java.util.*
+import kotlin.concurrent.thread
 
+@ExperimentalUnitApi
 fun main() = application {
-    val scope = rememberCoroutineScope()
 
-        val server = ServerSocket(9999)
-        println("Server is running on port ${server.localPort}")
-        val client = server.accept()
-        println("Client connected: ${client.inetAddress.hostAddress}")
-        val reader = Scanner(client.getInputStream())
-        val writer = client.getOutputStream()
+    val server = ServerSocket(9999)
+    println("Server is running on port ${server.localPort}")
+    val client = server.accept()
+    println("Client connected: ${client.inetAddress.hostAddress}")
+    val writer = client.getOutputStream()
 
     Window(onCloseRequest = ::exitApplication, title = "Server") {
         var inputValue by remember { mutableStateOf("") }
-        DesktopMaterialTheme {
-            Column(
-                modifier = Modifier.fillMaxSize().padding(20.dp),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.SpaceAround
-            ) {
-                Text("Welcome!")
-                Spacer(modifier = Modifier.weight(40f))
-                OutlinedTextField(value = inputValue, onValueChange = { inputValue = it })
-                Spacer(modifier = Modifier.weight(20f))
-                Button(
-                    onClick = {
-                        writer.write((inputValue + '\n').toByteArray(Charset.defaultCharset()))
-                    }
-                ) {
-                    Text("تایید")
+        Column(
+            modifier = Modifier.fillMaxSize().padding(20.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceAround
+        ) {
+            Text(
+                text = "خوش آمدید!",
+                style = TextStyle(
+                    fontSize = TextUnit(32f, TextUnitType.Sp),
+                    fontFamily = FontFamily(
+                        Font(resource = "vazir.ttf", weight = FontWeight.Bold)
+                    ),
+                    textAlign = TextAlign.Right,
+                    textDirection = TextDirection.Rtl
+                )
+            )
+            Spacer(modifier = Modifier.weight(40f))
+            OutlinedTextField(
+                value = inputValue,
+                onValueChange = {
+                    if (it.length <= 2)
+                        inputValue = it
+                },
+                label = {
+                    Text(
+                        text = "تعداد دانشکده",
+                        style = TextStyle(
+                            textAlign = TextAlign.Right,
+                            textDirection = TextDirection.Rtl,
+                            fontFamily = FontFamily(
+                                Font(resource = "vazir.ttf")
+                            )
+                        ),
+                    )
+                })
+            Spacer(modifier = Modifier.weight(20f))
+            Button(
+                shape = RoundedCornerShape(15.dp),
+                onClick = {
+                    writer.write((inputValue + '\n').toByteArray(Charset.defaultCharset()))
+                    runServer(server)
                 }
+            ) {
+                Text(
+                    text = "تایید",
+                    style = TextStyle(
+                        fontFamily = FontFamily(
+                            Font(resource = "vazir.ttf", weight = FontWeight.Bold)
+                        )
+                    )
+                )
             }
         }
     }
 
+}
 
+fun runServer(server: ServerSocket) {
+    while (true) {
+        val client = server.accept()
+        println("Client connected: ${client.inetAddress.hostAddress}")
+        thread { ClientHandler(client).run() }
+    }
 }
